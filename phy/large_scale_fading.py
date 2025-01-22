4032a0b363e1d103e644c75866841c350289460c
Zihao-Howard-Zhou <2562586184@qq.com>
2025-01-22 14:37:57 +0800

    add: support probabilistic LoS channel model

--- phy/large_scale_fading.py
+++ phy/large_scale_fading.py
@@ -1,7 +1,7 @@
 import math
 import logging
 from utils import config
-from utils.util_function import euclidean_distance
+from utils.util_function import euclidean_distance_3d, euclidean_distance_2d
 
 
 # config logging
@@ -46,15 +46,15 @@ def sinr_calculator(my_drone, main_drones_list, all_transmitting_drones_list):
 
                 logging.info('Main node is: %s, interference node is: %s, distance between them is: %s, main link' 
                              ' distance is: %s, interference link distance is: %s',
-                             transmitter_id, interference_id, euclidean_distance(transmitter.coords, interference.coords),
-                             euclidean_distance(transmitter.coords, receiver.coords),
-                             euclidean_distance(interference.coords, receiver.coords))
+                             transmitter_id, interference_id, euclidean_distance_3d(transmitter.coords, interference.coords),
+                             euclidean_distance_3d(transmitter.coords, receiver.coords),
+                             euclidean_distance_3d(interference.coords, receiver.coords))
 
                 interference_link_path_loss = general_path_loss(receiver, interference)
                 interference_power += transmit_power * interference_link_path_loss
         else:
             logging.info('No interference, main link distance is: %s',
-                         euclidean_distance(transmitter.coords, receiver.coords))
+                         euclidean_distance_3d(transmitter.coords, receiver.coords))
 
         sinr = 10 * math.log10(receive_power / (noise_power + interference_power))
         logging.info('The SINR of main link is: %s', sinr)
@@ -80,13 +80,55 @@ def general_path_loss(receiver, transmitter):
     fc = config.CARRIER_FREQUENCY
     alpha = 2  # path loss exponent
 
-    distance = euclidean_distance(receiver.coords, transmitter.coords)
+    distance = euclidean_distance_3d(receiver.coords, transmitter.coords)
 
     if distance != 0:
         path_loss = (c / (4 * math.pi * fc * distance)) ** alpha
     else:
         path_loss = 1
 
+    return path_loss
+
+def probabilistic_los_path_loss(receiver, transmitter):
+    """
+    probabilistic loss mode
+
+    References:
+        [1] A. Al-Hourani, S. Kandeepan and S. Lardner, "Optimal LAP Altitude for Maximum Coverage," in IEEE Wireless
+            Communications Letters, vol. 3, no. 6, pp. 569-572, 2014.
+        [2] J. Sabzehali, et al., "Optimizing number, placement, and backhaul connectivity of multi-UAV networks," in
+            IEEE Internet of Things Journal, vol. 9, no. 21, pp. 21548-21560, 2022.
+
+    :param receiver: the drone that receives the packet
+    :param transmitter: the drone that sends the packet
+    :return: path loss
+    """
+
+    c = config.LIGHT_SPEED
+    fc = config.CARRIER_FREQUENCY
+    alpha = 2  # path loss exponent
+    eta_los = 0.1
+    eta_nlos = 21
+    a = 4.88
+    b = 0.429
+
+    distance = euclidean_distance_3d(receiver.coords, transmitter.coords)
+    horizontal_dist = euclidean_distance_2d(receiver, transmitter)
+    vertical_dist = max(receiver.coords[2], transmitter.coords[2])
+
+    elevation_angle = math.atan(horizontal_dist / vertical_dist) * 180 / math.pi
+
+    los_prob = 1 / (1 + a * math.exp(-b * (elevation_angle - a)))
+    nlos_prob = 1 - los_prob
+
+    if distance != 0:
+        path_loss_los = ((c / (4 * math.pi * fc * distance)) ** alpha) * (10 ** (eta_los / 10))
+        path_loss_nlos = ((c / (4 * math.pi * fc * distance)) ** alpha) * (10 ** (eta_nlos / 10))
+    else:
+        path_loss_los = 1
+        path_loss_nlos = 1
+
+    path_loss = los_prob * path_loss_los + nlos_prob * path_loss_nlos
     return path_loss
 
 
